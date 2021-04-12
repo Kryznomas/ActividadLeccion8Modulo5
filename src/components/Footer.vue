c91da5b7fbed727000c9f3c4fda2e372a58b6f92
Kryznomas <kryztop@gmail.com>
2021-04-11 21:06:12 -0400

    Commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-footer elevated>
+    <q-toolbar class="justify-center">
+      <q-btn round color="brown-5" icon="directions" />
+      <q-btn round color="deep-orange" icon="edit_location" />
+      <q-btn round color="black" icon="my_location" />
+    </q-toolbar>
+  </q-footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
+
+<style lang="scss" scoped>
+.site-footer {
+  color: #fff;
+}
+</style>
+© 2021 GitHub, Inc.
