338355e67ec5551f68917face766cf816135baea
Kryznomas <kryztop@gmail.com>
2021-04-11 21:06:28 -0400

    commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-header elevated>
+    <q-toolbar>
+      <q-toolbar-title>
+        Prueba con Quasar
+      </q-toolbar-title>
+      <q-btn color="primary" glossy label="Home" href="#index" />
+      <q-btn color="standard" stretch flat label="Articulos" href="#articles" />
+      <q-btn color="standard" stretch flat label="Contacto" href="#footer" />
+    </q-toolbar>
+  </q-header>
+</template>
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-page class="flex flex-center">
+    <Navbar />
+    <div id="articles" class="articles q-pa-md row items-start justify-center q-gutter-md col-md-9">
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    </div > 
+    <Footer />
+  </q-page>
+</template>
+
+<script>
+import Navbar from "../components/Navbar.vue";
+import Card from "../components/Card.vue";
+import Footer from "../components/Footer.vue";
+
+export default {
+  name: 'PageIndex',
+  components: {
+    Navbar,
+    Card,
+    Footer
+  }
+}
+</script>
